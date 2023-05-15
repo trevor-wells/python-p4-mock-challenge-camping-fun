bfa97f0327c30db0669774e7fc89b81ff9a0bdf8
Ben Botsford <ben.botsford@flatironschool.com>
2023-05-15 11:16:39 -0500

    test from memory

--- server/app.py
+++ server/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
+
 from flask import Flask, request
 from flask_migrate import Migrate
 
 from models import db, Activity, Signup, Camper
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,9 +1,11 @@
 import json
-from os import environ
+import os
+os.environ['DB_URI'] = "sqlite:///:memory:"
+
 from flask import request
 
-from app import app
-from models import db, Activity, Signup, Camper
+from app import app, db
+from models import Activity, Signup, Camper
 
 class TestApp:
     '''Flask application in app.py'''
@@ -12,6 +14,9 @@ class TestApp:
         '''retrieves campers with GET requests to /campers.'''
 
         with app.app_context():
+
+            db.create_all()
+            
             clark = Camper(name="Clark Kent", age=9)
             db.session.add(clark)
             db.session.commit()
